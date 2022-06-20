34d73c887dc61d653c2d42d8ac8d467d070a28af
Andre Ribeiro <andrebahia7@gmail.com>
2022-06-20 16:05:39 -0300

    Criação API livros

---
+++ ChapterFST2/Controllers/LivroController.cs
@@ -0,0 +1,34 @@
+﻿using ChapterFST2.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChapterFST2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly LivroRepository _livrorepository;
+
+        public LivroController(LivroRepository livroRepository)
+        {
+            _livrorepository = livroRepository;
+        }
+
+        [HttpGet]
+        public  IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_livrorepository.Listar());
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception(e.Message);
+            }
+
+        }
+    }
+}
